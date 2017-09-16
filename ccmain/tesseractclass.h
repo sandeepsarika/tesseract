1696ce6f6dd53b9cff1db7e1aaa0258eb6a26857
Stefan Weil <sw@weilnetz.de>
2017-09-16 18:47:04 +0200

    Fix whitespace issues
    * Remove whitespace at line endings
    
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- ccmain/tesseractclass.h
+++ ccmain/tesseractclass.h
@@ -166,7 +166,7 @@ class Tesseract : public Wordrec {
   Tesseract();
   ~Tesseract();
 
-  // Return appropriate dictionary 
+  // Return appropriate dictionary
   Dict& getDict() override;
 
   // Clear as much used memory as possible without resetting the adaptive
