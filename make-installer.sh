3a2d27550c8566d1cecf3287858c3e22bc61d72b
Stefan Weil <sw@weilnetz.de>
2023-03-24 16:14:09 +0100

    Update installer build script
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- make-installer.sh
+++ make-installer.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-TAG=$(git describe | sed s/-.*//).$(date +%Y%m%d)
+TAG=$(git describe | sed s/^v// | sed s/.202[3-9].*// | sed s/-.*//).$(date +%Y%m%d)
 
 git tag -a v$TAG -m "Tesseract $TAG"
 
 ARCHS="i686 x86_64"
+ARCHS="x86_64"
+
+SRCDIR=$PWD
 
 ./autogen.sh
 
@@ -16,15 +19,23 @@ for ARCH in $ARCHS; do
   mkdir -p $BUILDDIR
   (
   cd $BUILDDIR
+
+  MINGW=/mingw64
+  MINGW_INSTALL=${PWD}${MINGW}
+  PKG_CONFIG_PATH=$MINGW/lib/pkgconfig
+  export PKG_CONFIG_PATH
+
   # Disable OpenMP (see https://github.com/tesseract-ocr/tesseract/issues/1662).
-  ../../../configure --disable-openmp --host=$HOST --prefix=/usr/$HOST CXX=$HOST-g++-posix CXXFLAGS="-fno-math-errno -Wall -Wextra -Wpedantic -g -O2"
-  make install-jars install training-install html prefix=$PWD/usr/$HOST
+  ../../../configure --disable-openmp --host=$HOST --prefix=$MINGW CXX=$HOST-g++-posix CXXFLAGS="-fno-math-errno -Wall -Wextra -Wpedantic -g -O2 -I$MINGW/include" LDFLAGS="-L$MINGW/lib"
+  make install-jars install training-install html prefix=${MINGW_INSTALL}
   (
-  cd $PWD/usr/$HOST/bin
+  cd ${MINGW_INSTALL}/bin
   for file in *.exe *.dll; do
     signcode $file
   done
   )
-  make winsetup prefix=$PWD/usr/$HOST SIGNCODE=signcode
+  mkdir -p dll
+  ln -sv $($SRCDIR/nsis/find_deps.py $MINGW_INSTALL/bin/*.exe $MINGW_INSTALL/bin/*.dll) dll/
+  make winsetup prefix=${MINGW_INSTALL} SIGNCODE=signcode
   )
 done
